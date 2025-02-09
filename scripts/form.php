90c7c39312175975336dd2125f140e5419b8f96d
Dev1 <Dev1@indigo.local>
2025-02-09 08:55:30 +0200

    some errors removed

--- scripts/form.php
+++ scripts/form.php
@@ -1,49 +1,51 @@
 <?php
 $errors = [];
-$id = $_POST['id'];
+$id = (int) $_POST['id'];
 $name = $_POST['name'];
 
+// reading config
+$config = file_get_contents(__DIR__ . '../config.json');
+$config = json_decode($config);
+if($config === null){
+    $errors['config'] = 'Error occured when reding config.json';
+}
+
 // id validation
-if (isset($id) && is_int($id)) {
+if (isset($id) && $id > 0) {
     $isAlreadyExist = false;
     foreach ($config['users'] as $userArray) {
-        if ($userArray['id'] === (int) $id) $isAlreadyExist;
+        if ($userArray['id'] === $id) $isAlreadyExist;
     }
 
     if ($isAlreadyExist) {
         $errors['id'] = 'This id already exists.';
     } else {
-        $id = (int) $id;
+        $id = $id;
     }
 } else {
     $errors['id'] = 'Invalid id number';
 }
 
 // name validation
-if (!isset($name) || !mb_strlen($name) >= 2) {
+if (!isset($name) || !mb_strlen($name) < 2) {
     $errors['name'] = 'Invalid name';
 }
 
 if (count($errors) === 0) {
-    $config = file_get_contents(__DIR__ . '../config.json');
-    $config = json_decode($config);
-    if ($config) {
-        $config['users'] = [
-            'id' => $id,
-            'name' => $name,
-        ];
-        file_put_contents(__DIR__ . '../config.json', $config);
-        if (false) {
-            $errors['config'] = 'Error occured when creating user';
-        }
-    } else {
-        $errors['config'] = 'Error occured when reding config.json';
+    $config['users'][] = [
+        'id' => $id,
+        'name' => $name,
+    ];
+    if (file_put_contents(__DIR__ . '../config.json', $config) === false) {
+        $errors['config'] = 'Error occured when creating user';
     }
 }
 
 if (count($errors) !== 0) {
-    $query = http_build_query(['errors' => json_encode($errors), 'name' => $name, 'id' => $id]);
-    header('Location: ' . __DIR__ . '/../index.php' . "?$query");
+    $query = http_build_query([
+        'errors' => json_encode($errors), 'name' => $name, 'id' => $id
+    ]);
+    header('Location: /php-config-editor/index.php' . "?$query");
     exit;
 }
 
